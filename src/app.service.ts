f54bf92bfa30b396adac677030ff7832f31d3959
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-04-01 16:54:25 +0200

    ci

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'API works! Docs page: /api-doc' ;
+    return 'API works! Swagger documentation page: /api-doc' ;
   }
 }
