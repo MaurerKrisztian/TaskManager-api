7e8cdfe8a32474f694f57d9c4544448392f534cf
krisztian.maurer <krisztian.maurer@metrix.co.hu>
2022-04-11 20:43:29 +0200

    schedules findall, remove

--- src/schedules/schedules.controller.ts
+++ src/schedules/schedules.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Delete, Get, Param} from '@nestjs/common';
 import { SchedulesService } from './schedules.service';
+import {SchedulesRepository} from "./schema/schedules.repository";
+import {IUser, User} from "../auth/auth.user.decorator";
 
 @Controller('schedules')
 export class SchedulesController {
-    constructor(private readonly schedulesService: SchedulesService) {}
+    constructor(private readonly schedulesService: SchedulesService, private readonly repo: SchedulesRepository) {}
 
-  @Get()
-    findAll() {
-        return ""
+    @Get()
+    findAll(@User() user: IUser) {
+        return this.repo.find({userId: user.id})
+    }
+
+    @Delete(':id')
+    delete(@User() user: IUser, @Param('id') id: string) {
+        return this.repo.remove(id);
     }
 
 }
